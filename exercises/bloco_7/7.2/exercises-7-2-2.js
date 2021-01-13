95d3658ca741ba76b03e41a00332986854086020
Luciano_Lodi <lrlodi@gmail.com>
2021-01-13 19:01:47 -0300

    Exercicio 2. Funcao orderModifier

--- exercises/bloco_7/7.2/exercises-7-2-2.js
+++ exercises/bloco_7/7.2/exercises-7-2-2.js
@@ -36,7 +36,6 @@ const order = {
 
 const customerInfo = (order) => {
   // Adicione abaixo as informações necessárias.
-  const orderKeys = Object.keys(order);
   const customerName = order.name;
   const phoneNumber = order.phoneNumber;
   const orderAddress = Object.values(order.address);
@@ -50,7 +49,34 @@ customerInfo(order);
 
 const orderModifier = (order) => {
   // Adicione abaixo as informações necessárias.
+  const newCustomer = Object.assign({}, order)
+  newCustomer.name = 'Luiz Silva';
+  newCustomer.order = {
+    pizza: {
+      muzzarella: {
+        amount: 1,
+        price: 20,
+      },
+      calabresa: {
+        amount: 1,
+        price:25,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 5,
+        amount: 1,
+      }
+    },
+    payment: {
+      total: 50,
+    }
+  }
+  const pizzaFlavors = Object.keys(Object.assign({}, (newCustomer.order).pizza));
+  const drinks = newCustomer.order.drinks.coke.type;
 
+  console.log(`Olá ${newCustomer.name}, o total de seu pedido de ${pizzaFlavors} e ${drinks} é R$${newCustomer.order.payment.total}.`)
 }
 
 orderModifier(order);
